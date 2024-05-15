3890eacff8e318841f158962e74cd4ad0b1e932d
NekruzAsh <nekruzashrapov16@gmail.com>
2024-05-14 21:49:20 -0400

    started to work on the navbar and the landing page.

---
+++ app/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id="home" className="w-full h-screen text-center relative">
+        <div className="max-w-[1240px] w-full h-full mx-auto p-4 flex justify-center items-center">
+                hello
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+
+//components
+
+export default function Home() {
+  return (
+    <main>
+      
+    </main>
+  );
+}
