406e7921cdb6ed98cf085c89400f7a59014cc0c0
NekruzAsh <nekruzashrapov16@gmail.com>
2024-05-16 21:08:28 -0400

    updated navbar items and minor color changes.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,43 +2,47 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="navbar z-[100] fixed bg-[#2c2e3a] backdrop-filter backdrop-blur-xl bg-opacity-70 top-5 left-0 right-0 w-[380px] md:w-[600px] lg:w-[1000px] h-7 mx-auto rounded-lg shadow-md">
+    <div className="navbar z-[100] fixed bg-[#2c2e3a] backdrop-filter backdrop-blur-xl bg-opacity-70 top-5 left-0 right-0 w-[380px] md:w-[600px] lg:w-[1000px] mx-auto rounded-lg shadow-md">
+    {/* Navbar for Mobile Screens */}
     <div className="navbar-start">
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a>Item 1</a></li>
+          <li><a>About Us</a></li>
           <li>
-            <a>Parent</a>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
+            <a href="">Membership</a>
           </li>
-          <li><a>Item 3</a></li>
+          <li><a>Meeting Slides</a></li>
+          <li><a>IEEE Student Magazine</a></li>
+          <li><a>Events</a></li>
+          <li><a>Links</a></li>
+          <li><a>Officers</a></li>
         </ul>
       </div>
-      <a className="btn btn-ghost text-xl">IEEE</a>
+      <a className=''>
+        <img src='/images/ieee-1.png' 
+        width={120}
+        height={120}
+        className='rounded-md'
+        />
+        
+      </a>
     </div>
+    {/* Navbar for Computer Screens */}
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
-        <li><a>Item 3</a></li>
+      <li className='text-xs hover:text-[#66FCF1]'><a>About Us</a></li>
+          <li className='text-xs hover:text-[#66FCF1]'>
+            <a href="">Membership</a>
+          </li>
+          <li className='text-xs hover:text-[#66FCF1]'><a>Meeting Slides</a></li>
+          <li className='text-xs hover:text-[#66FCF1]'><a>IEEE Student Magazine</a></li>
+          <li className='text-xs hover:text-[#66FCF1]'><a>Events</a></li>
+          <li className='text-xs hover:text-[#66FCF1]'><a>Links</a></li>
+          <li className='text-xs hover:text-[#66FCF1]'><a>Officers</a></li>
       </ul>
-    </div>
-    <div className="navbar-end">
-      <a className="btn">Button</a>
     </div>
   </div>
   )
