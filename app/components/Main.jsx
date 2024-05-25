3c3df12c43863c2a81595e27e5bccf1738b9981d
NekruzAsh <nekruzashrapov16@gmail.com>
2024-05-24 22:56:39 -0400

    updated the landing page.

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center relative">
-        <div className="max-w-[1240px] w-full h-full mx-auto p-4 flex justify-center items-center">
-                hello
+      <div className="max-w-[1240px] w-full h-full mx-auto p-4 flex justify-center items-center">
+        <div className="grid grid-cols-2 ">
+          <div className="">
+            <h1>Welcome to IEEE</h1>
+          </div>
+          <div className="">
+            <h2>Meeting Dates</h2>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
