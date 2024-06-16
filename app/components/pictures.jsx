6ddf0f0fcfe600654e6436548262c46f4cf727fc
NekruzAsh <nekruzashrapov16@gmail.com>
2024-06-15 22:34:21 -0400

    changed the positions of the iphone mockups.

--- app/components/pictures.jsx
+++ app/components/pictures.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-const pictures = () => {
+const Pictures = () => {
   return (
-    <div className="w-full h-full items-center flex justify-center">
+    <div className="w-full h-full flex items-center justify-center">
       <div className="max-w-[1280px] mb-[21rem]">
         <div className="absolute top-[45rem] bottom-0 left-[12rem] right-[12rem] mt-20 grid grid-cols-4 gap-4">
+          {/* First Phone */}
           <div className="mockup-phone h-[36rem] border-gray-400 hidden lg:block">
             <div className="camera"></div>
             <div className="display">
-              <div className="artboard artboard-demo phone-1">
+              <div className="artboard artboard-demo phone-1 flex justify-center items-center">
                 <img
-                  className="rounded-lg h-[36rem] w-[22rem] hidden lg:block"
+                  className="rounded-lg h-[34rem] w-[20rem] hidden lg:block"
                   src="/images/img1.jpg"
                   alt=""
                 />
@@ -18,24 +19,27 @@ const pictures = () => {
             </div>
           </div>
 
-          <div className="mockup-phone h-[37rem] border-gray-400 hidden lg:block">
+          {/* Second Phone with offset */}
+          <div className="mockup-phone h-[36rem] border-gray-400 hidden lg:block mt-[-2rem]">
             <div className="camera"></div>
             <div className="display">
-              <div className="artboard artboard-demo phone-1">
+              <div className="artboard artboard-demo phone-1 flex justify-center items-center">
                 <img
-                  className="rounded-lg h-[36rem] w-[24rem] hidden lg:block"
+                  className="rounded-lg h-[34rem] w-[20rem] hidden lg:block"
                   src="/images/img3.jpg"
                   alt=""
                 />
               </div>
             </div>
           </div>
-          <div className="mockup-phone h-[37rem] border-gray-400">
+
+          {/* Third Phone */}
+          <div className="mockup-phone h-[36rem] border-gray-400">
             <div className="camera"></div>
             <div className="display">
-              <div className="artboard artboard-demo phone-1">
+              <div className="artboard artboard-demo phone-1 flex justify-center items-center">
                 <img
-                  className="rounded-lg h-[36rem] w-[22rem]"
+                  className="rounded-lg h-[34rem] w-[20rem]"
                   src="/images/img2.jpg"
                   alt=""
                 />
@@ -43,12 +47,13 @@ const pictures = () => {
             </div>
           </div>
 
-          <div className="mockup-phone h-[37rem] border-gray-400">
+          {/* Fourth Phone with offset */}
+          <div className="mockup-phone h-[36rem] border-gray-400 mt-[-2rem]">
             <div className="camera"></div>
             <div className="display">
-              <div className="artboard artboard-demo phone-1">
+              <div className="artboard artboard-demo phone-1 flex justify-center items-center">
                 <img
-                  className="rounded-lg h-[35rem] w-[22rem]"
+                  className="rounded-lg h-[34rem] w-[20rem]"
                   src="/images/uc.jpg"
                   alt=""
                 />
@@ -61,4 +66,4 @@ const pictures = () => {
   );
 };
 
-export default pictures;
+export default Pictures;
